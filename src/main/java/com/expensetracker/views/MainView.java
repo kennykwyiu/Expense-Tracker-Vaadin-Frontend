3741577efe92ea6a58963eda85542387908bbe7d
kennyyiu <kenny.kwyiu@gmail.com>
2026-02-22 23:53:30 +0800

    add MainView

---
+++ src/main/java/com/expensetracker/views/MainView.java
@@ -0,0 +1,18 @@
+package com.expensetracker.views;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+/**
+ * Main application layout with navigation sidebar.
+ */
+public class MainView extends AppLayout {
+
+}
