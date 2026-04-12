c220eca28a9abd73f8cee3658d81243313acb9bd
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-12 22:23:21 +0800

    MonthlyBalanceResponse - add variables

--- src/main/java/com/expensetracker/dto/MonthlyBalanceResponse.java
+++ src/main/java/com/expensetracker/dto/MonthlyBalanceResponse.java
@@ -1,4 +1,17 @@
 package com.expensetracker.dto;
 
+import java.math.BigDecimal;
+
 public class MonthlyBalanceResponse {
+    private Integer id;
+    private Integer userId;
+    private Integer year;
+    private Integer month;
+    private BigDecimal lastMonthBalance;
+    private BigDecimal incomeThisWeek;
+    private BigDecimal expenseBudget;
+    private BigDecimal currentBalance;
+    private String createdAt;
+    private String updatedAt;
+
 }
