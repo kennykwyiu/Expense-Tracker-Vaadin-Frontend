97c2f101826712914d9ef7df0c69f4886e61d38d
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-12 22:25:27 +0800

    UpdateMonthlyBalanceRequest - add variables

--- src/main/java/com/expensetracker/dto/UpdateMonthlyBalanceRequest.java
+++ src/main/java/com/expensetracker/dto/UpdateMonthlyBalanceRequest.java
@@ -1,4 +1,9 @@
 package com.expensetracker.dto;
 
+import java.math.BigDecimal;
+
 public class UpdateMonthlyBalanceRequest {
+    private BigDecimal lastMonthBalance;
+    private BigDecimal incomeThisWeek;
+    private BigDecimal expenseBudget;
 }
