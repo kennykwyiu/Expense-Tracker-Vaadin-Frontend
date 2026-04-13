3e35862a07309f4e5fe52bb72bae70eaf1e21469
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-13 23:55:10 +0800

    UpdateMonthlyBalanceRequest - add Getters and Setters

--- src/main/java/com/expensetracker/dto/UpdateMonthlyBalanceRequest.java
+++ src/main/java/com/expensetracker/dto/UpdateMonthlyBalanceRequest.java
@@ -26,4 +26,30 @@ public class UpdateMonthlyBalanceRequest {
         this.incomeThisWeek = incomeThisWeek;
         this.expenseBudget = expenseBudget;
     }
+
+    // Getters and Setters
+
+    public BigDecimal getLastMonthBalance() {
+        return lastMonthBalance;
+    }
+
+    public void setLastMonthBalance(BigDecimal lastMonthBalance) {
+        this.lastMonthBalance = lastMonthBalance;
+    }
+
+    public BigDecimal getIncomeThisWeek() {
+        return incomeThisWeek;
+    }
+
+    public void setIncomeThisWeek(BigDecimal incomeThisWeek) {
+        this.incomeThisWeek = incomeThisWeek;
+    }
+
+    public BigDecimal getExpenseBudget() {
+        return expenseBudget;
+    }
+
+    public void setExpenseBudget(BigDecimal expenseBudget) {
+        this.expenseBudget = expenseBudget;
+    }
 }
