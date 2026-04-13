a70710280372538be85b9f09d402a6b783ad9706
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-13 23:58:53 +0800

    JacksonConfig - add annotations and comment

--- src/main/java/com/expensetracker/config/JacksonConfig.java
+++ src/main/java/com/expensetracker/config/JacksonConfig.java
@@ -1,4 +1,11 @@
 package com.expensetracker.config;
 
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Jackson configuration for Java 8 date/time support.
+ * Enables proper serialization and deserialization of LocalDateTime, LocalDate, etc.
+ */
+@Configuration
 public class JacksonConfig {
 }
