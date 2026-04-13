28a2d18dd540f894c720b5ed19ec765a602b4aaf
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-13 23:54:32 +0800

    UpdateMonthlyBalanceRequest - add comment

--- src/main/java/com/expensetracker/dto/UpdateMonthlyBalanceRequest.java
+++ src/main/java/com/expensetracker/dto/UpdateMonthlyBalanceRequest.java
@@ -1,7 +1,10 @@
 package com.expensetracker.dto;
 
 import java.math.BigDecimal;
-
+/**
+ * DTO for updating Monthly Balance via API.
+ * Used to send balance update requests to the backend.
+ */
 public class UpdateMonthlyBalanceRequest {
     private BigDecimal lastMonthBalance;
     private BigDecimal incomeThisWeek;
