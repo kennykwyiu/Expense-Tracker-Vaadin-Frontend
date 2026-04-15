4a7ef7b76ca60747e681d10965c947fbef83ad12
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-15 23:34:53 +0800

    BalanceSummaryCard - add openAddIncomeDialog

--- src/main/java/com/expensetracker/components/BalanceSummaryCard.java
+++ src/main/java/com/expensetracker/components/BalanceSummaryCard.java
@@ -3,10 +3,16 @@ package com.expensetracker.components;
 import com.expensetracker.dto.MonthlyBalanceResponse;
 import com.expensetracker.util.Logger;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.progressbar.ProgressBar;
+import com.vaadin.flow.component.textfield.BigDecimalField;
 
 import java.math.BigDecimal;
 import java.util.function.Consumer;
@@ -32,4 +38,37 @@ public class BalanceSummaryCard extends Div {
     private Consumer<BigDecimal> onBudgetUpdate;
 
     private MonthlyBalanceResponse currentBalance;
+    /**
+     * Open dialog to add income
+     */
+    private void openAddIncomeDialog() {
+        logger.info("Opening add income dialog");
+
+        Dialog dialog = new Dialog();
+        dialog.setHeaderTitle("Add Income");
+        dialog.setWidth("400px");
+
+        FormLayout form = new FormLayout();
+        BigDecimalField incomeField = new BigDecimalField("Income Amount");
+//        incomeField.setMin(0);
+        incomeField.setHelperText("Enter income amount");
+        form.add(incomeField);
+
+        Button saveBtn = new Button("Add", e -> {
+            if (incomeField.getValue() != null && onIncomeUpdate != null) {
+                logger.info("Adding income: " + incomeField.getValue());
+                onIncomeUpdate.accept(incomeField.getValue());
+                dialog.close();
+            }
+        });
+        saveBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        Button cancelBtn = new Button("Cancel", e -> dialog.close());
+
+        HorizontalLayout buttons = new HorizontalLayout(saveBtn, cancelBtn);
+        buttons.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+
+        dialog.add(form, buttons);
+        dialog.open();
+    }
 }
