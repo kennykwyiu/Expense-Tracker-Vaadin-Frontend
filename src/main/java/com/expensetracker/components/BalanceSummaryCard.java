1fdcfde32acfdebf975f6230cbbfb8f9f7b565bf
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-15 23:32:53 +0800

    BalanceSummaryCard - add variables

--- src/main/java/com/expensetracker/components/BalanceSummaryCard.java
+++ src/main/java/com/expensetracker/components/BalanceSummaryCard.java
@@ -1,7 +1,15 @@
 package com.expensetracker.components;
 
+import com.expensetracker.dto.MonthlyBalanceResponse;
 import com.expensetracker.util.Logger;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.progressbar.ProgressBar;
+
+import java.math.BigDecimal;
+import java.util.function.Consumer;
 
 /**
  * Balance Summary Card component for displaying monthly financial summary.
@@ -10,4 +18,18 @@ import com.vaadin.flow.component.html.Div;
 public class BalanceSummaryCard extends Div {
 
     private final Logger logger = new Logger(BalanceSummaryCard.class);
+
+    private final Span lastMonthBalanceLabel = new Span();
+    private final Span incomeThisWeekLabel = new Span();
+    private final Span expenseBudgetLabel = new Span();
+    private final Span currentBalanceLabel = new Span();
+    private final ProgressBar budgetProgressBar = new ProgressBar();
+
+    private final Button addIncomeBtn = new Button(VaadinIcon.PLUS.create());
+    private final Button editBudgetBtn = new Button(VaadinIcon.EDIT.create());
+
+    private Consumer<BigDecimal> onIncomeUpdate;
+    private Consumer<BigDecimal> onBudgetUpdate;
+
+    private MonthlyBalanceResponse currentBalance;
 }
