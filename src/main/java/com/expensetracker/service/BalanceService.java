9d1bffaa2613ccaf8b5959615e0241710da97f78
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-19 20:32:09 +0800

    BalanceService - add annotations and comment

--- src/main/java/com/expensetracker/service/BalanceService.java
+++ src/main/java/com/expensetracker/service/BalanceService.java
@@ -1,4 +1,10 @@
 package com.expensetracker.service;
 
+import org.springframework.stereotype.Service;
+
+/**
+ * Service for calling Balance API endpoints
+ */
+@Service
 public class BalanceService {
 }
