0cdc920a0c5462aad61538b60f0caadef2ef04ae
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-19 20:36:17 +0800

    BalanceService - add getMonthlyBalance

--- src/main/java/com/expensetracker/service/BalanceService.java
+++ src/main/java/com/expensetracker/service/BalanceService.java
@@ -6,7 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Value;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 /**
  * Service for calling Balance API endpoints
@@ -26,6 +29,22 @@ public class BalanceService {
      */
     public MonthlyBalanceResponse getMonthlyBalance(Integer year, Integer month) throws Exception {
         logger.info("Getting monthly balance for " + year + "-" + month);
-
+        
+        String url = backendApiUrl + "/balance/" + year + "/" + month;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .header("Content-Type", "application/json")
+                .build();
+        
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        if (response.statusCode() == 200) {
+            logger.info("Balance retrieved successfully");
+            return objectMapper.readValue(response.body(), MonthlyBalanceResponse.class);
+        } else {
+            logger.error("Failed to get balance. Status: " + response.statusCode());
+            throw new Exception("Failed to get balance: " + response.statusCode());
+        }
     }
 }
